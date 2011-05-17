118eee4d47df33395b52f6d4cbb95d50e17420fc
Kevin Cantu <me@kevincantu.org>
2011-05-17 00:46:43 -0700

    broken, but time for sleep...

--- blake.hs
+++ blake.hs
@@ -16,7 +16,7 @@ initialValues :: [Word32]
 initialValues = [ 0x6a09e667, 0xbb67ae85,
                   0x3c6ef372, 0xa54ff53a,
                   0x510e527f, 0x9b05688c,
-                  0x1f83d9ab, 0x5be0cd19 ]
+                  0x1f83d9ab, 0x5be0cd19 ] --ok
 
 
 -- BLAKE-256 constants
@@ -28,21 +28,21 @@ constants = [ 0x243f6a88, 0x85a308d3,
               0x452821e6, 0x38d01377,
               0xbe5466cf, 0x34e90c6c,
               0xc0ac29b7, 0xc97cd0dd,
-              0x3f84d5b5, 0xb5470917 ]
+              0x3f84d5b5, 0xb5470917 ] --ok
 
 
 -- BLAKE-256 permutations of 0 to 15
 sigma :: [[ Int ]]
-sigma = [[ 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 ],
-         [ 14,10,4,8,9,15,13,6,1,12,0,2,11,7,5,3 ],
-         [ 11,8,12,0,5,2,15,13,10,14,3,6,7,1,9,4 ],
-         [ 7,9,3,1,13,12,11,14,2,6,5,10,4,0,15,8 ],
-         [ 9,0,5,7,2,4,10,15,14,1,11,12,6,8,3,13 ],
-         [ 2,12,6,10,0,11,8,3,4,13,7,5,15,14,1,9 ],
-         [ 12,5,1,15,14,13,4,10,0,7,6,3,9,2,8,11 ],
-         [ 13,11,7,14,12,1,3,9,5,0,15,4,8,6,2,10 ],
-         [ 6,15,14,9,11,3,0,8,12,2,13,7,1,4,10,5 ],
-         [ 10,2,8,4,7,6,1,5,15,11,9,14,3,12,13,0 ]]
+sigma = [[  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 ], 
+         [ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 ], 
+         [ 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 ], 
+         [  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 ], 
+         [  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 ], 
+         [  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 ], 
+         [ 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 ], 
+         [ 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 ], 
+         [  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 ], 
+         [ 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13,  0 ]] --ok?
 
 
 -- replace items in a list
@@ -62,6 +62,7 @@ replace newWords words =
 
 -- BLAKE-256 round function
 -- apply multiple G computations for a single round
+-- PENDING: THERE IS AN ERROR IN THIS FUNCTION...
 blakeRound messageblock stateV r = 
 
         -- define each Gi in a round as (i, cell numbers)
@@ -87,11 +88,11 @@ blakeRound messageblock stateV r =
                 in
             
                 -- compute the round
-                let a'  = a  + b  + (messageblock !! sigmaf (2*i)) `xor` (constants !! sigmaf (2*i + 1))
+                let a'  = a  + b  + ((messageblock !! sigmaf (2*i)) `xor` (constants !! sigmaf (2*i + 1)))
                     d'  = (d `xor` a') `rotate` (-16) 
                     c'  = c + d' 
                     b'  = (b `xor` c') `rotate` (-12) 
-                    a'' = a' + b' + (messageblock !! sigmaf (2*i + 1)) `xor` (constants !! sigmaf (2*i))
+                    a'' = a' + b' + ((messageblock !! sigmaf (2*i + 1)) `xor` (constants !! sigmaf (2*i)))
                     d'' = (d' `xor` a'') `rotate` (-8) 
                     c'' = c' + d'' 
                     b'' = (b' `xor` c'') `rotate` (-7)
@@ -117,7 +118,7 @@ compress :: [Word32] -> [Word32] -> [Word32] -> [Word32] -> [Word32]
 compress h m s t =
 
     -- initialize state, 16 words
-    let v = (++) h $ zipWith xor (s ++ [t!!0, t!!0, t!!1, t!!1]) (take 8 constants)
+    let v = h ++ (zipWith xor (s ++ [t!!0, t!!0, t!!1, t!!1]) (take 8 constants))
     in
 
     -- do 14 rounds on this messageblock
@@ -170,8 +171,11 @@ type Counter = [Word32]
 -- BLAKE-256 padding
 -- blocks of 512 bits, padded, as 32 bit words 
 -- (tupled with counter words)
---blocks :: Word64 -> [Word8] -> [( [Word32], [Word32] )]
-blocks :: Word64 -> [Word8] -> [( Counter, MessageBlock )]
+{- OK:
+    *Main> blocks 0 [0]
+    [([8388608,0,0,0,0,0,0,0,0,0,0,0,0,1,8,0],[8,0])]
+-}
+blocks :: Word64 -> [Word8] -> [( [Word32], [Word32] )]
 blocks counter s = 
 
     -- do this before calling blocks?
@@ -253,3 +257,20 @@ main = B.readFile "blake.hs"
 
 
 
+-- compress round 1
+--  let r1 = (\ s h (m,t) -> compress h m s t) [0,0,0,0] initialValues $ head $ blocks 0 $ B.unpack $ B.pack [0]
+-- ERROR
+
+
+-- initial values
+-- let i = (\s h (m,t) ->  h ++ (zipWith xor (s ++ [t!!0, t!!0, t!!1, t!!1]) (take 8 constants))) [0,0,0,0] initialValues $ head $ blocks 0 $ B.unpack $ B.pack [0]
+-- OK?
+
+
+-- one round
+-- let b1 = (\(m,t) -> blakeRound m i 1) $ head $ blocks 0 $ B.unpack $ B.pack [0]
+--
+--
+--
+--
+--

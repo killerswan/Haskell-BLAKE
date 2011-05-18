81a48fc9c8ac958d75237747d4f8505eb50bdf6f
Kevin Cantu <me@kevincantu.org>
2011-05-18 07:06:20 -0700

    cleaning up

--- blake.hs
+++ blake.hs
@@ -32,17 +32,17 @@ constants = [ 0x243f6a88, 0x85a308d3,
 
 
 -- BLAKE-256 permutations of 0 to 15
-sigma :: [[ Int ]]
-sigma = [[  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 ], 
-         [ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 ], 
-         [ 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 ], 
-         [  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 ], 
-         [  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 ], 
-         [  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 ], 
-         [ 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 ], 
-         [ 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 ], 
-         [  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 ], 
-         [ 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13,  0 ]]
+sigmaTable :: [[ Int ]]
+sigmaTable = [[  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 ], 
+              [ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 ], 
+              [ 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 ], 
+              [  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 ], 
+              [  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 ], 
+              [  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 ], 
+              [ 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 ], 
+              [ 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 ], 
+              [  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 ], 
+              [ 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13,  0 ]]
 
 
 -- replace items in a list
@@ -78,22 +78,18 @@ blakeRound messageblock state round =
         in
 
         -- perform a given Gi within the round function
-        let fG v ii = 
-                let [a,b,c,d] = map (v !!) (g !! ii)
-                in
+        let fG state ii = 
+                let 
+                    [a,b,c,d] = map (state !!) (g !! ii)
 
-                -- get sigma
-                let sigmaf n = sigma !! (round `mod` 10) !! n
-                in
+                    -- get sigma
+                    sigma n = sigmaTable !! (round `mod` 10) !! n
 
-                let messageword n = messageblock !! sigmaf n
-                in
-    
-                let constant n = constants !! sigmaf n
-                in
+                    messageword n = messageblock !! sigma n
+                    constant    n = constants    !! sigma n
             
-                -- compute the round
-                let a'  = a  + b  + (messageword (2*ii) `xor` constant (2*ii + 1))
+                    -- compute the round
+                    a'  = a  + b  + (messageword (2*ii) `xor` constant (2*ii + 1))
                     d'  = (d `xor` a') `rotate` (-16) 
                     c'  = c + d' 
                     b'  = (b `xor` c') `rotate` (-12) 
@@ -105,8 +101,7 @@ blakeRound messageblock state round =
 
                 -- return a copy of the state list
                 -- with each of the computed cells replaced 
-                replace (zip (g !! ii) [a'', b'', c'', d'']) v
-                --replace (zip (g !! ii) [a, b, c, d]) v               -- turn this into identity
+                replace (zip (g !! ii) [a'', b'', c'', d'']) state
         in
 
         foldl' fG state [0..7]

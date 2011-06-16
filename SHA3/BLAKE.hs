f85cbe6abec1698ca6505b63c3bdd0122b2fd575
Kevin Cantu <me@kevincantu.org>
2011-06-15 23:32:20 -0700

    ok...

--- SHA3/BLAKE.hs
+++ SHA3/BLAKE.hs
@@ -113,46 +113,61 @@ applyGTo4 state messageblock round (ii, cells) =
 
 -- BLAKE-256 bit shifting
 -- BLAKE-512 bit shifting
---
+
+
+
+
+-- i, cells for each Gi
+-- columns
+g03 = 
+        [ (0, [0,4,8,12]),
+          (1, [1,5,9,13]), 
+          (2, [2,6,10,14]), 
+          (3, [3,7,11,15]) ] 
+-- diagonals
+g47 = 
+        [ (4, [0,5,10,15]),
+          (5, [1,6,11,12]), 
+          (6, [2,7,8,13]), 
+          (7, [3,4,9,14]) ] 
+
+
+-- rotate a 2d list
+rotate4 m = 
+    map (!! 0) m :
+    map (!! 1) m : 
+    map (!! 2) m : 
+    map (!! 3) m : []
+
+
 -- BLAKE-256 round function
 -- apply multiple G computations for a single round
 blakeRound mode messageblock state round = 
 
-        let rotate4 m = 
-                map (!! 0) m :
-                map (!! 1) m : 
-                map (!! 2) m : 
-                map (!! 3) m : []
-        in
+        let 
+            g = applyGTo4 state messageblock round
 
-        let bs = (applyGTo4 state messageblock round)
-        in
 
-        let makeColumns state = 
-                let cols = map bs
-                             [ (0, [0,4,8,12]), -- 4 columns
-                               (1, [1,5,9,13]), 
-                               (2, [2,6,10,14]), 
-                               (3, [3,7,11,15]) ] 
+            makeColumns state = 
+                let 
+--                    g = applyGTo4 state messageblock round
+                    cols = map g g03
                 in
                 concat $ rotate4 cols
-        in
-        let makeDiagonals state = 
-                let diags = map (applyGTo4 state messageblock round) -- WHY GOD WHY??
-                --let diags = map bs
-                              [ (4, [0,5,10,15]),  -- 4 diagonals
-                                (5, [1,6,11,12]), 
-                                (6, [2,7,8,13]), 
-                                (7, [3,4,9,14]) ] 
 
+
+            makeDiagonals state = 
+                let 
+                    g = applyGTo4 state messageblock round
+                    diags = map g g47
                     cols = rotate4 diags
                     shiftRowRight n row = drop j row ++ take j row
-                                       where j = length row - n
+                                            where j = length row - n
                 in
                 concat [ shiftRowRight 0 (cols !! 0),
                          shiftRowRight 1 (cols !! 1),
                          shiftRowRight 2 (cols !! 2),
-                         shiftRowRight 3 (cols !! 3) ]
+                         shiftRowRight 3 (cols !! 3)  ]
 
         in
 

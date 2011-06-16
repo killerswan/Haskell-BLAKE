f3c738bead855ba61b30efe853e399ba254ca84e
Kevin Cantu <me@kevincantu.org>
2011-06-15 22:00:35 -0700

    constants for 512

--- SHA3/BLAKE.hs
+++ SHA3/BLAKE.hs
@@ -4,10 +4,10 @@
 -- use at your own risk.
 
 module SHA3.BLAKE ( blake256,
-                    initialValues,
-                    initialState,
-                    blocks,
-                    blakeRound,
+                    --initialValues256,
+                    --initialState,
+                    --blocks,
+                    --blakeRound,
                     blake512--,
                     --blake224,
                     --blake384
@@ -21,42 +21,66 @@ import Data.Maybe -- needed for isJust, fromJust
 
 
 -- BLAKE-256 initial values
-initialValues :: [Word32]
-initialValues = [ 0x6a09e667, 0xbb67ae85,
-                  0x3c6ef372, 0xa54ff53a,
-                  0x510e527f, 0x9b05688c,
-                  0x1f83d9ab, 0x5be0cd19 ]
+initialValues256 :: [Word32]
+initialValues256 = 
+    [ 0x6a09e667, 0xbb67ae85,
+      0x3c6ef372, 0xa54ff53a,
+      0x510e527f, 0x9b05688c,
+      0x1f83d9ab, 0x5be0cd19 ]
 
 
 -- BLAKE-256 constants
-constants :: [Word32]
-constants = [ 0x243f6a88, 0x85a308d3,
-              0x13198a2e, 0x03707344,
-              0xa4093822, 0x299f31d0,
-              0x082efa98, 0xec4e6c89,
-              0x452821e6, 0x38d01377,
-              0xbe5466cf, 0x34e90c6c,
-              0xc0ac29b7, 0xc97c50dd,
-              0x3f84d5b5, 0xb5470917 ]
+constants256 :: [Word32]
+constants256 = 
+    [ 0x243f6a88, 0x85a308d3,
+      0x13198a2e, 0x03707344,
+      0xa4093822, 0x299f31d0,
+      0x082efa98, 0xec4e6c89,
+      0x452821e6, 0x38d01377,
+      0xbe5466cf, 0x34e90c6c,
+      0xc0ac29b7, 0xc97c50dd,
+      0x3f84d5b5, 0xb5470917 ]
+
+
+-- BLAKE-512 initial values
+initialValues512 :: [Word64]
+initialValues512 = 
+    [ 0x6A09E667F3BCC908, 0xBB67AE8584CAA73B,
+      0x3C6EF372FE94F82B, 0xA54FF53A5F1D36F1,
+      0x510E527FADE682D1, 0x9B05688C2B3E6C1F,
+      0x1F83D9ABFB41BD6B, 0x5BE0CD19137E2179 ]
+
+
+-- BLAKE-512 constants
+constants512 :: [Word64]
+constants512 = 
+    [ 0x243F6A8885A308D3, 0x13198A2E03707344,
+      0xA4093822299F31D0, 0x082EFA98EC4E6C89,
+      0x452821E638D01377, 0xBE5466CF34E90C6C,
+      0xC0AC29B7C97C50DD, 0x3F84D5B5B5470917,
+      0x9216D5D98979FB1B, 0xD1310BA698DFB5AC,
+      0x2FFD72DBD01ADFB7, 0xB8E1AFED6A267E96,
+      0xBA7C9045F12C7F99, 0x24A19947B3916CF7,
+      0x0801F2E2858EFC16, 0x636920D871574E69 ]
 
 
 -- BLAKE-256 permutations of {0..15}
 sigmaTable :: [[ Int ]]
-sigmaTable = [[  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 ], 
-              [ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 ], 
-              [ 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 ], 
-              [  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 ], 
-              [  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 ], 
-              [  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 ], 
-              [ 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 ], 
-              [ 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 ], 
-              [  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 ], 
-              [ 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13,  0 ]]
+sigmaTable =
+    [[  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 ], 
+     [ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 ], 
+     [ 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 ], 
+     [  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 ], 
+     [  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 ], 
+     [  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 ], 
+     [ 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 ], 
+     [ 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 ], 
+     [  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 ], 
+     [ 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13,  0 ]]
 
 
 -- BLAKE-256 round function
 -- apply multiple G computations for a single round
--- PENDING: THERE IS AN ERROR IN THIS FUNCTION...
 blakeRound mode messageblock state round = 
 
         -- perform a given Gi within the round function
@@ -75,7 +99,7 @@ blakeRound mode messageblock state round =
                     sigma n = sigmaTable !! (round `mod` 10) !! n
 
                     messageword n = messageblock !! sigma n
-                    constant    n = constants    !! sigma n
+                    constant    n = constants256 !! sigma n
             
                     -- compute the round
                     a'  = a  + b  + (messageword (2*ii) `xor` constant (2*ii + 1))
@@ -133,7 +157,7 @@ blakeRound mode messageblock state round =
 -- rather than reverse it in `blocks` below, i changed the numbering here
 initialState h s t = 
     h ++ 
-    zipWith xor (s ++ [t!!1, t!!1, t!!0, t!!0]) (take 8 constants)
+    zipWith xor (s ++ [t!!1, t!!1, t!!0, t!!0]) (take 8 constants256)
 
 
 -- BLAKE-256 compression of one message block
@@ -274,7 +298,7 @@ blocks mode message =
 --blake :: Int -> Salt -> [Word8] -> Hash
 blake mode salt message =
     let ivs = case mode of
-                256 -> initialValues
+                256 -> initialValues256
         
         compress' h (m,t) = compress mode h m salt t
     in

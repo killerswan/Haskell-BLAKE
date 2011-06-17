2e25aebc3494a355f17ce36d40251354bf49f388
Kevin Cantu <me@kevincantu.org>
2011-06-17 00:00:21 -0700

    towards 256 and 512 bit options

--- Main.hs
+++ Main.hs
@@ -10,20 +10,30 @@ import System
 import Text.Printf
 
 
-hex xs = (printf "%08x") =<< xs
+-- print a list of numbers as a hex string
+hex32 ws = (printf "%08x") =<< ws
+hex64 ws = (printf "%016x") =<< ws
 
 
-printHash salt path = 
+-- print out the BLAKE hash followed by the file name
+-- generic
+printHashX hex blake salt path = 
     do
         msg <- B.readFile path
-        hash <- return $ hex $ blake256 salt (B.unpack msg)
+        hash <- return $ hex $ blake salt (B.unpack msg)
         putStrLn $ hash ++ " *" ++ path
+
+-- 256
+printHash256 = printHashX hex32 blake256
+
+-- 512
+printHash512 = printHashX hex64 blake512
 
 
 main = 
     do 
         args <- getArgs
-        sequence $ map (printHash [0,0,0,0]) args
+        sequence $ map (printHash256 [0,0,0,0]) args
 
     -- TODO: add option parsing so we can check when a -c flag and file of hashes is given
 

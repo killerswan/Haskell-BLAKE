0184310e2fd8588be1787d4b804592ac7d34f551
Kevin Cantu <me@kevincantu.org>
2011-06-16 23:16:52 -0700

    simplifying the printing

--- Main.hs
+++ Main.hs
@@ -7,35 +7,16 @@ import SHA3.BLAKE
 import Data.Bits
 import qualified Data.ByteString.Lazy as B
 import System
+import Text.Printf
 
 
-hexchar n word = case 0xF .&. (word `shift` (-4 * n)) of
-                0x0 -> '0'
-                0x1 -> '1'
-                0x2 -> '2'
-                0x3 -> '3'
-                0x4 -> '4'
-                0x5 -> '5'
-                0x6 -> '6'
-                0x7 -> '7'
-                0x8 -> '8'
-                0x9 -> '9'
-                0xa -> 'a'
-                0xb -> 'b'
-                0xc -> 'c'
-                0xd -> 'd'
-                0xe -> 'e'
-                0xf -> 'f'
-
-
-hex32 w = map hc [7,6,5,4,3,2,1,0]
-        where hc n = hexchar n w
+hex xs = (printf "%08x") =<< xs
 
 
 printHash salt path = 
     do
         msg <- B.readFile path
-        hash <- return $ hex32 =<< blake256 salt (B.unpack msg)
+        hash <- return $ hex $ blake256 salt (B.unpack msg)
         putStrLn $ hash ++ " *" ++ path
 
 

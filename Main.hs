308a2089ef7d491ca038b7b2420a3b6edac21478
Kevin Cantu <me@kevincantu.org>
2011-06-18 02:16:21 -0700

    wrestled GetOpt into submission

--- Main.hs
+++ Main.hs
@@ -3,11 +3,77 @@
 -- A naive implementation of the Blake cryptographic hash: 
 -- use at your own risk.
 
+module Main (main) where
+
 import SHA3.BLAKE
 import Data.Bits
 import qualified Data.ByteString.Lazy as B
 import System
+import List
+import IO
+import Char
 import Text.Printf
+import Control.Monad
+--import System.Console.CmdArgs.Implicit
+--import System.Console.CmdLib
+import System.Console.GetOpt
+
+data Options = Options { help      :: Bool
+                       , check     :: Bool
+                       , algorithm :: Integer
+                       , salt      :: Integer 
+                       }
+
+defaultOpts :: Options
+defaultOpts = Options { help = False
+                      , check = False
+                      , algorithm = 256
+                      , salt = 0
+                      }
+
+options :: [ OptDescr (Options -> IO Options) ]
+options = [ Option "a" ["algorithm"] 
+                   (ReqArg
+                        (\arg opt -> let alg = read arg :: Integer
+                                     in case alg of 
+                                            x | x == 256 || x == 512 -> return opt { salt = alg }
+                                            otherwise -> error "please choose a working algorithm size")
+                        "BITS")
+                   "256, 512, 224, 384 (default: 256)"
+
+          , Option "c" ["check"] 
+                   (NoArg $ \opt -> return opt { check = True })
+                   "positive integer salt (default: 0)"
+
+          , Option "s" ["salt"] 
+                   (ReqArg
+                        (\arg opt -> let i = read arg :: Integer
+                                     in 
+                                     if i >= 0
+                                     then return opt { salt = i }
+                                     else error "please specify a positive salt")
+                        "SALT")
+                   "positive integer salt (default: 0)"
+
+          , Option "h" ["help"] 
+                   (NoArg  $ \_ -> do
+                        prg <- getProgName
+                        hPutStrLn stderr $ usageInfo prg options
+                        exitWith ExitSuccess)
+                   "display this help and exit"
+
+          , Option "v" ["version"] 
+                   (NoArg $ \_ -> do
+                        me <- getProgName
+                        hPutStrLn stderr $ me ++ " version A"
+                        exitWith ExitSuccess)
+                   "display version and exit"
+          ]
+
+
+-- TODO: may need to add error handling 
+--       for excessively long inputs per the BLAKE paper)
+
 
 
 -- print a list of numbers as a hex string
@@ -30,10 +96,22 @@ printHash256 = printHashX hex32 blake256
 printHash512 = printHashX hex64 blake512
 
 
+
+
 main = 
     do 
         args <- getArgs
-        sequence $ map (printHash256 [0,0,0,0]) args
 
-    -- TODO: add option parsing so we can check when a -c flag and file of hashes is given
+        let (actions, nonOptions, errors) = getOpt RequireOrder options args
+        opts <- foldl (>>=) (return defaultOpts) actions
+        
+        let Options { check = check,
+                      algorithm = algorithm,
+                      salt = salt} = opts
+        sequence $ map (printHash256 [0,0,0,0]) nonOptions
+
+
+-- TODO: implement these options and stdin
+--       implement check mode
+        
 
